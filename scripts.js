8cc6b1c610d27e00cef1fc0844a3313641a0752b
loaming <loaming372@gmail.com>
2020-07-10 17:07:30 +0300

    media and scripts

---
+++ scripts.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+    let open = false;
+  document.querySelector("#mobile_menu").addEventListener("click",function(){
+   if(open){
+      document.querySelector(".mobile_nav").classList.add("nav_close");
+      document.querySelector(".mobile_nav").classList.remove("nav_open");
+    }else{
+      document.querySelector(".mobile_nav").classList.add("nav_open");
+      document.querySelector(".mobile_nav").classList.remove("nav_close");
+    }
+    open=!open;
+  })}
+  
